8dfbdab06a85bebda01fa035811f136f28122db8
animeint <adliraji@yahoo.co.id>
2020-08-18 19:03:52 +0700

    Project1

---
+++ ToDoApps/Models/TodoItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoApps.Models
+{
+    public class TodoItem
+    {
+        [Key]
+        public int ItemId { get; set; }
+        public string Title { get; set; }
+        //  public int Priority { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime? DueDate { get; set; }
+        public int? PercentCompleted { get; set; }
+        public bool? TrackProgress { get; set; }
+        public bool? RemindOnDelay { get; set; }
+        public bool? IsAccomplished { get; set; }
+    }
+}
---
+++ ToDoApps/Models/TrashedTaskItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoApps.Models
+{
+    public class TrashedTaskItem
+    {
+        public int ItemId { get; set; }
+        public string Title { get; set; }
+       // public int Priority { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime? DueDate { get; set; }
+        public int? PercentCompleted { get; set; }
+        public bool? TrackProgress { get; set; }
+        public bool? RemindOnDelay { get; set; }
+        public bool? IsAccomplished { get; set; }
+
+    }
+}
